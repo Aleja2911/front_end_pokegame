f7a3819862c04cfb648cc1ffdd3888642fa25d4c
Laura Ningel <laura.ningel@gmail.com>
2020-11-05 16:29:52 +0100

    detail page imgs

--- src/Pages/PokemonDetails.js
+++ src/Pages/PokemonDetails.js
@@ -1,24 +1,34 @@
-import React, { useState, useEffect} from 'react'; 
+import React, { useState, useEffect } from "react";
 
-const PokemonDetails = ( { singlePokemon } ) => {
-    const [pokemonDetail, setPokemonDetail] = useState([]);
-    
-    useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${singlePokemon.toLowerCase()}`)
-            .then((res) => res.json())
-            .then((data) => setPokemonDetail(data))
-            .catch((error) => console.log(error.message))
-    })
+const PokemonDetails = ({ singlePokemon }) => {
+  const [pokemonImg, setPokemonImg] = useState({
+    front: "",
+    back: "",
+    animated: "",
+  });
 
-    return ( 
-        <div>
-            {pokemonDetail && pokemonDetail.map((pokemonInfo) => {
-                return (
-                    <img src={pokemonInfo.sprites.front_default}/> 
-                ) 
-            })}
-        </div>
-     );
-}
- 
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${singlePokemon.toLowerCase()}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemonImg((prev) => ({
+          ...prev,
+          front: data.sprites.front_default,
+          back: data.sprites.back_default,
+          animated:
+            data.sprites.versions["generation-v"]["black-white"].animated
+              .front_default,
+        }));
+      });
+  }, []);
+
+  return (
+    <div>
+      <img src={pokemonImg.front} style={{ width: 200 }} />
+      <img src={pokemonImg.animated} style={{ width: 80 }} />
+      <img src={pokemonImg.back} style={{ width: 200 }} />
+    </div>
+  );
+};
+
 export default PokemonDetails;
